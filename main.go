ebbb8208e91ca337932b90a7e32c62088eaa0101
y.zauzina <y.zauzina@tinkoff.ru>
2024-09-05 23:05:23 +0100

    go 123

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"reflect"
 )
 
 func getProfile(w http.ResponseWriter, r *http.Request) {
@@ -11,12 +9,8 @@ func getProfile(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	httpMux := reflect.ValueOf(http.DefaultServeMux).Elem()
-	finList := httpMux.FieldByIndex([]int{1})
-	fmt.Println(finList)
 
 	http.HandleFunc("GET /my", getProfile)
 	http.HandleFunc("GET /profile", getProfile)
 	http.ListenAndServe(":8091", nil)
-
 }
